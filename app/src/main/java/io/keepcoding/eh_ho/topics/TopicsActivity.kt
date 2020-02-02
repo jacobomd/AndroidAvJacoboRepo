9edc92e0af06e7516fba4685155ac381bfb4da26
jacobomd <javoluis71@gmail.com>
2020-02-02 19:04:01 +0100

    Add example implementation navigation drawer

--- app/src/main/java/io/keepcoding/eh_ho/topics/TopicsActivity.kt
+++ app/src/main/java/io/keepcoding/eh_ho/topics/TopicsActivity.kt
@@ -2,7 +2,14 @@ package io.keepcoding.eh_ho.topics
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import io.keepcoding.eh_ho.*
 import io.keepcoding.eh_ho.data.Topic
 import io.keepcoding.eh_ho.data.UserRepo
@@ -11,21 +18,45 @@ import io.keepcoding.eh_ho.posts.EXTRA_TOPIC_ID
 import io.keepcoding.eh_ho.posts.EXTRA_TOPIC_TITLE
 import io.keepcoding.eh_ho.posts.PostsActivity
 
+
 const val TRANSACTION_CREATE_TOPIC = "create_topic"
 
 
-class TopicsActivity : AppCompatActivity(), TopicsFragment.TopicsInteractionListener, CreateTopicFragment.CreateTopicInterationListener {
+class TopicsActivity : AppCompatActivity(),
+    TopicsFragment.TopicsInteractionListener,
+    CreateTopicFragment.CreateTopicInterationListener,
+NavigationView.OnNavigationItemSelectedListener {
+
+
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_topics)
 
+        //setSupportActionBar(toolbarTopicsToolbar)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .add(R.id.fragmentContainer, TopicsFragment())
                 .commit()
         }
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0, 0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
 
     }
 
@@ -60,5 +91,21 @@ class TopicsActivity : AppCompatActivity(), TopicsFragment.TopicsInteractionList
         startActivity(intent)
         finish()
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_topics -> {
+                Toast.makeText(this, "nav_topics clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_latest_news -> {
+                Toast.makeText(this, "nav_latest_news clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_logout -> {
+                Toast.makeText(this, "nav_logout clicked", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true    }
 
 }
